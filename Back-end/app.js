5123050d54f693a9f8ee7cbf4cef857ea190b035
Jhodman Contreras <48294817+JhodmanC@users.noreply.github.com>
2019-10-29 11:24:13 -0500

    Frond-end and Open-Dates
    Se arreglo el login y ya esta disponible el uso del API de datos abiertos.

--- Back-end/app.js
+++ Back-end/app.js
@@ -4,6 +4,7 @@
 var express = require('express'); // Express: Framework HTTP para Node.js
 var routes = require('./routes'); // Dónde tenemos la configuración de las rutas
 var path = require('path');
+var datos= require('./routes/datos');
 
 var mongoose = require('mongoose'); // Mongoose: Libreria para conectar con MongoDB
 var passport = require('passport'); // Passport: Middleware de Node que facilita la autenticación de usuarios
@@ -13,10 +14,19 @@ var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var errorhandler = require('errorhandler');
+var Request = require("request");
+
+
+
+var async = require('express-async-await');
+var fetch = require('node-fetch');
 
 // Importamos el modelo usuario y la configuración de passport
 require('./models/user');
 require('./passport')(passport);
+
+
+
 
 // Conexión a la base de datos de MongoDB que tenemos en local
 mongoose.connect('mongodb://localhost:27017/passport-example', function(err, res) {
@@ -66,6 +76,11 @@ if ('development' == app.get('env')) {
 // del modulo 'routes'
 app.get('/', routes.index);  //cuando se ponga la pagina va a ir a la ruta el fichero index
 
+//haciendo pruebas para que renderice index de una
+//app.get('/',function(req,res) {
+//  res.sendFile('index.html',{ root: '.' });
+//});
+
 
 /* Rutas de Passport */
 // Ruta para desloguearse
@@ -88,7 +103,94 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook',
   { successRedirect: '/', failureRedirect: '/login' }
 ));
 
+//rutas datos
+
+//let url = 'https://www.datos.gov.co/resource/9ghi-7xv6.json';
+
+
+// Soporte para bodies codificados en jsonsupport.
+var url 	= "https://www.datos.gov.co/resource/9ghi-7xv6.json"
+app.use(bodyParser.json());
+// Soporte para bodies codificados
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/api/datos', function(req, res) {
+	Request({
+	    url: url,
+	    json: false
+	}, function (error, response, body) {
+ 
+	    if (!error && response.statusCode === 200) {
+	    	// Pintamos la respuesta JSON en navegador.
+	        res.send(body) 
+	    }
+	})
+});
+
+//tambien funciona pero este aplica filtrado:
+//https://luisjordan.net/node-js/web-services-con-node-js-express-creacion-de-api-rest/
+
+//var url 	= "https://www.datos.gov.co/resource/9ghi-7xv6.json"
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.get('/api/datos/:nomarea', function(req, res) {
+//  var itemNomarea = req.params.nomarea;
+//	Request({
+//	    url: url+itemNomarea,
+//	    json: false
+//	}, function (error, response, body) {
+ 
+//	    if (!error && response.statusCode === 200) {
+	    	// Pintamos la respuesta JSON en navegador.
+//	        res.send(body) 
+//	    }
+//	})
+//});
+// Fin del que funciona con el parametro de busqueda que lo trae
+
+
+// SI FUNCIONA! no tocar
+//app.get('/api/datos', function(req, res) {
+//  Request('https://www.datos.gov.co/resource/9ghi-7xv6.json', function(err, body){
+//    res.json(body); //res is the response object, and it passes info back to client side
+//});
+//});
+  
+  
+  //let traerJson = () => {
+    //return fetch(url);
+  //}
+
+  //let mandarJson = async () => {
+   // let eljson = await traerJson();
+   // let respuesta = eljson.json();
+    //console.log('RespuestaDatos: ' + respuesta);
+
+  //}
+
+  //return res.json(path.join(url+''));
+  //---------------------------------------------------
+  //mandarJson();
+  //let jsondatos = await fetch(url);
+  //console.log('exito '+ jsondatos.json());
+  //res.send(jsondatos);
+ //res.end();
+  //res.send(jsondatos);
+  //res.send('respond with a resource');
+//}
+//);
+
+app.use(express.static(url,{extensions:['json']}));
+app.use(function(req,res){
+  res.status(404);
+  res.json({
+    error:{
+      code: 404
+    }
+  })
+})
+
 // Inicio del servidor
 app.listen(app.get('port'), function(){
   console.log('Aplicación Express escuchando en el puerto ' + app.get('port'));
+  
 });
